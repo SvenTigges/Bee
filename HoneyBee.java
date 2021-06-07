dbf9b23c6fd087c934e766dcee1c49b3bde32a84
Your Name <sven.tigges@web.de>
2021-06-07 12:18:18 +0200

    Update

---
+++ HoneyBee.java
@@ -0,0 +1,10 @@
+public class HoneyBee {
+    
+    public String fly() {
+        return "I believe, I can fly!";
+    }
+
+    public String doYourJob() {
+        return "I´m a Honeybee!";
+    }
+}
